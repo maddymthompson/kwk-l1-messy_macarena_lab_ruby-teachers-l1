25e54fc0627e59417b547aa2ee92368e98fd535e
maddymthompson <madeline.thompson.17@tfacorps.org>
2018-07-26 03:42:57 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -8,8 +8,14 @@ puts ""
 # Step 1: GOOD
 puts "Step 1: Right arm out in front of you, palm facing down."
 
+4. "Turn your left palm up."
+5. "Place your right hand on your left shoulder."
+6. "Place your left hand on your right shoulder."
+7. "Bring your right hand up and around to the back of your head."
+8. "Bring your left hand up and around to the back your head."
+
 # Step 2: FIXME
-puts "Step 2: Turn your right palm up."
+puts "Left arm out in front of you, palm facing down."
 
 # Step 3: FIXME
 puts "Step 3: Place your right hand on your left shoulder."
